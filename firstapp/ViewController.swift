e3dffc6266c831e2845069e33a61c3fad82d4a90
Cuong T Luong <c3luong@acs-wlh2015-24.ucsd.edu>
2016-04-07 11:10:09 -0700

    Initial Commit

---
+++ firstapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstapp
+//
+//  Created by Cuong T Luong on 4/7/16.
+//  Copyright (c) 2016 Cuong T Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
